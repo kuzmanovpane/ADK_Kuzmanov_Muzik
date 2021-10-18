d57baf6bcabb9961018345b666b126e4fc174494
kuzmanovpane <pane.kuzmanov@fsv.cvut.cz>
2021-10-18 18:31:40 +0200

    Revert "changed push button"
    This reverts commit 4df5cc3f52aaeae78fd1adec92f6f1247fc533dd.

--- PointInPolygonAlgorithms/widget.cpp
+++ PointInPolygonAlgorithms/widget.cpp
@@ -32,6 +32,7 @@ void Widget::on_pushButtonAnalyze_clicked()
     //Analyze position of the point and vertex
     QPoint q = ui->Canvas->getPoint();
     std::vector<QPolygon> polygons = ui->Canvas->getPolygons();
+   // std::vector<QPoint> pol1 = ui->Canvas->getPolygon();
     std::vector<QPoint> pol;
 
 
@@ -49,32 +50,35 @@ void Widget::on_pushButtonAnalyze_clicked()
     //Get position
     Algorithms a;
     int pos = 10; //random number
+    //int pos1 = 0;
 
     //Ray Crossing algorithm
    if (ui->comboBox->currentIndex() == 0)
     {
        pos = a.getPositionRayCrossing(q, pol);
+       //pos1 = a.getPositionRayCrossing(q1, pol1);
     }
-
    //Winding number algorithm
     else
     {
        pos = a.getPositionWinding(q, pol);
+      // pos1 = a.getPositionWinding(q1, pol1);
     }
     pol.clear();
+    //pol1.clear();
 
 
     //Print results
-    if (pos == 1)
+    if (pos == 1) // || pos1 == 1)
     {
         ui->label->setText("Point is inside");
 
     }
-    else if (pos == 0)
+    else if (pos == 0) // || pos1 == )
     {
         ui->label->setText("Point is outside");
     }
-    else if (pos == -1)
+    else if (pos == -1) // || pos1 == -1)
     {
         ui->label->setText("Point is on the border");
     }
